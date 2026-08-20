f096a3742c5028cfd6da8bde07171f0c10710aaf
Bhushan Salkar <salkarbhushan57@gmail.com>
2026-08-20 13:51:48 +0530

    Updated project

--- loop.js
+++ loop.js
@@ -1,8 +1,25 @@
-function lo(event)
-{  let tab=document.getElementById("lo");
-    for(let i=1;i<11;i++)
-    {
-        tab.innerHTML+="27 x "+i+" = "+(27*i)+"<br>";
+function lo(event) {
+    let tab = document.getElementById("lo");
+    for (let i = 1; i < 11; i++) {
+        tab.innerHTML += "27 x " + i + " = " + (27 * i) + "<br>";
     }
 }
+
+function nat_num(event) {
+    let nat = Number(document.getElementById("nat").value);
+    let nat_op = document.getElementById("nat_op");
+    let nat_sum = 0;
+    for (let i = 1; i <= nat; i++) {
+        nat_sum += i;
+    }
+    nat_op.innerHTML = nat_sum;
+}
+
+function indexpage(event) {
+    window.location.href = "firstpage.html";
+}
+
 lo(new Event("submit"));
+document.getElementById("nat_button").addEventListener("click", nat_num);
+
+document.getElementById("indexpage").addEventListener("click", indexpage);
--- practice_set1.js
+++ practice_set1.js
@@ -1,60 +1,111 @@
 function op1(event) {
     event.preventDefault();
+
     let a = "Harry";
     let b = 6;
     let c = a + b;
-    document.getElementById("op1").innerHTML =
+
+    let output = document.getElementById("op1");
+
+    output.innerHTML =
         "The value of a is " + a +
-        "<br> The value of b is " + b +
-        "<br>The sum is:" + c;
+        "<br>The value of b is " + b +
+        "<br>The sum is: " + c;
+
+    output.classList.add("show");
 }
+
 
 function op2(event) {
     event.preventDefault();
+
     let a = 6;
     let b = 3;
     let c = a + b;
-    document.getElementById("op2").innerHTML =
-        " The value of a is " + a +
-        "<br> The value of b is " + b +
-        " <br> The datatype of c is : " + typeof c;
+
+    let output = document.getElementById("op2");
+
+    output.innerHTML =
+        "The value of a is " + a +
+        "<br>The value of b is " + b +
+        "<br>The datatype of c is: " + typeof c;
+
+    output.classList.add("show");
 }
+
 
 function op3(event) {
-    const a = { name: "bhushan", age: 23, isPrincipal: true };
-    //a=45;
-    let mes = " the constant a is an object and we can change the properties of the object but we cannot reassign the object to a new value";
+    event.preventDefault();
 
-    document.getElementById("op3").innerHTML =
-        "The value of a is " + JSON.stringify(a) +
-        "<br> the message is:" + mes;
+    const a = {
+        name: "bhushan",
+        age: 23,
+        isPrincipal: true
+    };
+
+    let output = document.getElementById("op3");
+
+    output.innerHTML =
+        "The value of a is: " +
+        JSON.stringify(a) +
+        "<br><br>You can change the properties of a const object, but you cannot reassign the object.";
+
+    output.classList.add("show");
 }
+
 
 function op4(event) {
-    const a1 = { name: "bhushan", age: 23, isPrincipal: true };
-    const a2 = { name: "bhushan", age: 23, isPrincipal: true };
-    a2["friend"] = "harsh";  //new property added to a2
-    a2["age"] = 24;          //edited
-    
-    document.getElementById("op4").innerHTML =
-        "The value of a1 is <br>" + JSON.stringify(a1) +
-        "<br>The value of a2 is:" + JSON.stringify(a2);
+    event.preventDefault();
+
+    const a1 = {
+        name: "bhushan",
+        age: 23,
+        isPrincipal: true
+    };
+
+    const a2 = {
+        name: "bhushan",
+        age: 23,
+        isPrincipal: true
+    };
+
+    a2["friend"] = "harsh";
+    a2["age"] = 24;
+
+    let output = document.getElementById("op4");
+
+    output.innerHTML =
+        "a1:<br>" + JSON.stringify(a1) +
+        "<br><br>a2:<br>" + JSON.stringify(a2);
+
+    output.classList.add("show");
 }
 
+
 function op5(event) {
+    event.preventDefault();
+
     const dict = {
-        appreciate: "recognize the full worth of ",
+        appreciate: "recognize the full worth of",
         ataraxia: "state of freedom",
         yakka: "hard work"
     };
-    document.getElementById("op5").innerHTML =
-        "the value of appriaciation:  " + dict.appreciate +
-        "<br> the value of ataraxia:  " + dict.ataraxia +
-        "<br> the value of yakka:  " + dict.yakka;
-}
-op1(new Event("submit"));
-op2(new Event("submit"));
-op3(new Event("submit"));
-op4(new Event("submit"));
-op5(new Event("submit"));
 
+    let output = document.getElementById("op5");
+
+    output.innerHTML =
+        "Appreciate: " + dict.appreciate +
+        "<br>Ataraxia: " + dict.ataraxia +
+        "<br>Yakka: " + dict.yakka;
+
+    output.classList.add("show");
+}
+
+
+// BUTTON EVENTS
+
+document.getElementById("op1_button").addEventListener("click", op1);
+document.getElementById("op2_button").addEventListener("click", op2);
+document.getElementById("op3_button").addEventListener("click", op3);
+document.getElementById("op4_button").addEventListener("click", op4);
+document.getElementById("op5_button").addEventListener("click", op5);
