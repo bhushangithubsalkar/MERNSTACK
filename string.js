34f47438a9217a5805786aa4c8d4b924685d1948
Bhushan Salkar <salkarbhushan57@gmail.com>
2026-08-24 22:59:08 +0530

    update 1.5

--- string.js
+++ string.js
@@ -1,6 +1,13 @@
 document.getElementById("string_btn").addEventListener("click", function () {
     let a = document.getElementById("string_inp").value;
     let b = document.getElementById("string_op");
-    
-    
+
+    b.innerHTML = "Length: " + a.length;
+
+    b.innerHTML += "<br>Uppercase: " + a.toUpperCase();
+
+    b.innerHTML += "<br>Lowercase: " + a.toLowerCase();
+
+    b.innerHTML += "<br>Reverse: " + a.split("").reverse().join("");
+
 });
