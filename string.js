af55e8847ef3e4cefb36e6667b867cf576f4619c
Bhushan Salkar <salkarbhushan57@gmail.com>
2026-09-09 15:41:39 +0530

    update 1.7.3

--- string.js
+++ string.js
@@ -1,24 +1,45 @@
 document.getElementById("string_btn").addEventListener("click", function () {
 
     let a = document.getElementById("string_inp").value;
+    let c = document.getElementById("string_op");
+    c.innerHTML += "Length: " + a.length;
+});
+
+
+document.getElementById("string_upper_btn").addEventListener("click", function () {
+    let a = document.getElementById("string_inp").value;
+    let c = document.getElementById("string_op");
+    c.innerHTML = "<br>Uppercase: " + a.toUpperCase();
+});
+
+
+document.getElementById("string_lower_btn").addEventListener("click", function () {
+    let a = document.getElementById("string_inp").value;
+    let c = document.getElementById("string_op");
+    c.innerHTML += "<br>Lowercase: " + a.toLowerCase();
+});
+
+document.getElementById("string_reverse_btn").addEventListener("click", function () {
+    let a = document.getElementById("string_inp").value;
+    let c = document.getElementById("string_op");
+    c.innerHTML += "<br>Reverse: " + a.split("").reverse().join("");
+});
+
+document.getElementById("string_conc_btn").addEventListener("click", function () {
+    let a = document.getElementById("string_inp").value;
     let b = document.getElementById("string_inp1").value;
     let c = document.getElementById("string_op");
-
-    
-    c.innerHTML = "Length: " + a.length;
-
+    let d=a+" "+b;
+    c.innerHTML += "<br>Conconatenate: " + d;
 });
-document.get
-c.innerHTML += "<br>Uppercase: " + a.toUpperCase();
 
-    c.innerHTML += "<br>Lowercase: " + a.toLowerCase();
+document.getElementById("string_new_btn").addEventListener("click", function () {
+    let d = document.getElementById("string_inp").value;
+let sen = ` <br> the name is ${d}`;
+let c = document.getElementById("string_op");
+c.innerHTML += sen;
+});
 
-    c.innerHTML += "<br>Reverse: " + a.split("").reverse().join("");
 
-    c.innerHTML += "<br>Concatenation: " + (a + b);
 
-    let d="pratham";
-    let sen=` <br> the name is ${d}`;
-    c.innerHTML+=sen;
-
-    let fruit='Bhushan\'s'; //it is the example of excape character
+let fruit = 'Bhushan\'s'; //it is the example of excape character
